cdc203eb9c5de01a9b6c72fec95110d7c6aa0eec
Vladimir Gumennikov <>
2019-08-20 18:16:29 +1000

    на первое ревью

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const svgmap = document.querySelector('#svgmap');
 const buttonPlus = document.querySelector('#button-plus');
 const buttonMinus = document.querySelector('#button-minus');
+const buttonNormal = document.querySelector('#button-normal');
 const arrayPoints = [];
 const viewBox = {x: 0, y: 0};
 
 let scale = 1;
 
 function clickOnMap(event){
-    console.log(event);
-    //hideS();
-    // if(!event.target.classList.contains("point-of-interest"))
-    //     return; //щёлкнули куда попало, но не по кругляшку магазина
-    
-    let idx = getIndexPointByPointId(event.target.id);
+    if(!event.target.classList.contains("point-of-interest"))
+         return; //щёлкнули куда попало, но не по кругляшку магазина
+
+    let idx = getIndexPointByPointId(event.target.parentNode.id);
     if(idx===-1) // так не должно быть. но вдруг?
         return;
     if(arrayPoints[idx].s == null){ // нет доп информации по точке
-        alert("А вот тут сэкономили и информацию не подготовили");
+        console.log("А вот тут сэкономили и информацию не подготовили");
+        console.log(arrayPoints[idx].p);
         return;
     }
     arrayPoints[idx].s.style.visibility = "visible";
@@ -41,13 +41,20 @@ function clickOnMinus(){
     rescaleSVG();
 }
 
+function clickOnNormal(){
+    scale =  1;
+    rescaleSVG();
+}
+
+
 function rescaleSVG(){
-    svgmap.setAttribute("width", `${viewBox.x*scale}`);
-    svgmap.setAttribute("height", `${viewBox.y*scale}`);
+    svgmap.setAttribute("width", viewBox.x*scale);
+    svgmap.setAttribute("height", viewBox.y*scale);
+
     for (let i = 0; i < arrayPoints.length; i++) {
-        arrayPoints[i].p.setAttribute("transform", `translate(${-arrayPoints[i].fX*(1/scale-1)}, ${-arrayPoints[i].fY*(1/scale-1)}) scale(${1/scale})`);
+        arrayPoints[i].p.setAttribute("transform", "translate(" + -arrayPoints[i].fX*(1/scale-1) + ", " +  -arrayPoints[i].fY*(1/scale-1) + ") scale(" + 1/scale + ")");
         if(arrayPoints[i].s!=null){
-            arrayPoints[i].s.setAttribute("transform", `translate(${-arrayPoints[i].fX*(1/scale-1)}, ${-arrayPoints[i].fY*(1/scale-1)}) scale(${1/scale})`);
+            arrayPoints[i].s.setAttribute("transform", "translate(" + -arrayPoints[i].fX*(1/scale-1) + ", " +  -arrayPoints[i].fY*(1/scale-1) + ") scale(" + 1/scale + ")");
         }
     }
 }
@@ -55,8 +62,10 @@ function rescaleSVG(){
 function initArrayPoints(){
     for (let i = 0; i < 19 ; i++) {
         let point = {};
-        point.p = svgmap.querySelector(`#p_x5F_${i+1}`); //ссылка на объект "точка"
-        point.s = svgmap.querySelector(`#s_x5F_${i+1}`); //ссылка на объект "указатель с буквой S"
+
+        point.p = svgmap.querySelector("#p_x5F_" + (i+1)); //ссылка на объект "точка"
+        point.s = svgmap.querySelector("#s_x5F_" + (i+1)); //ссылка на объект "указатель с буквой S"
+
         point.x = point.p.querySelector('circle').getAttribute("cx"); //координата X центра точки строкой
         point.y = point.p.querySelector('circle').getAttribute("cy"); //координата Y центра точки строкой
         point.fX = parseFloat(point.x); //координата X центра точки флоатом
@@ -73,19 +82,16 @@ function initArrayPoints(){
 
 
 function getIndexPointByPointId(pointID){
-    console.log(pointID);
     for (let i = 0; i < arrayPoints.length; i++) {
-        console.log(`-- ${i} - ${arrayPoints[i].p.id}`);
         if(arrayPoints[i].p.id === pointID)
             return i;
     }
-    console.log("-----");
     return -1;
 }
 
 function initViewBox()
 {
-    viewBoxCoord = svgmap.getAttribute("viewBox").split(" ");
+    let viewBoxCoord = svgmap.getAttribute("viewBox").split(" ");
 
     viewBox.x = parseFloat(viewBoxCoord[2]);
     viewBox.y= parseFloat(viewBoxCoord[3]);
@@ -94,19 +100,9 @@ function initViewBox()
 
 buttonPlus.addEventListener("click", clickOnPlus);
 buttonMinus.addEventListener("click", clickOnMinus);
-
+buttonNormal.addEventListener("click", clickOnNormal);
 svgmap.addEventListener("click", clickOnMap);
-
-// let elements = document.querySelectorAll('.point-of-interest');
-//
-// for (let elem of elements) {
-//     elem.addEventListener("mouseover", clickOnMap);
-//     elem.addEventListener("mouseout", hideS);
-// }
-
 
 initArrayPoints();
 initViewBox();
 rescaleSVG();
-//console.log(arrayPoints);
-//hideS();
