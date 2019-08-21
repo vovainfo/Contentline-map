7288742bccd3d4de34ee343da372635b15a781f1
Vladimir Gumennikov <vova@vova.info>
2019-08-22 00:34:26 +1000

    разбираюсь с createElementNS

--- script.js
+++ script.js
@@ -107,6 +107,37 @@ function initScale(){
     scale = scrollbox.clientWidth/viewBox.x;
 }
 
+function addPoint(x,y,index){
+    const strPoint = `
+                <linearGradient gradientTransform="matrix(1 0 0 -1 0 1095.89)" gradientUnits="userSpaceOnUse" id="mySVGID_${index}_1" x1="${x-13.2}" x2="${x+13.2}" y1="100" y2="100">
+                    <stop offset="0" style="stop-color:#B3916D"></stop>
+                    <stop offset="0.23" style="stop-color:#A07D6E"></stop>
+                    <stop offset="0.68" style="stop-color:#6F4A6F"></stop>
+                    <stop offset="1" style="stop-color:#4A2370"></stop>
+                </linearGradient>
+                <circle class="point-of-interest" cx="${x}" cy="${y}" r="13.2"></circle>
+                <circle class="point-of-interest st5" cx="${x}" cy="${y}" r="12.2"></circle>
+                <linearGradient gradientTransform="matrix(1 0 0 -1 0 1095.89)" gradientUnits="userSpaceOnUse" id="mySVGID_${index}_2" x1="${x-9.8}" x2="${x+9.8}" y1="100" y2="100">
+                    <stop offset="0" style="stop-color:#B3916D"></stop>
+                    <stop offset="0.23" style="stop-color:#A07D6E"></stop>
+                    <stop offset="0.68" style="stop-color:#6F4A6F"></stop>
+                    <stop offset="1" style="stop-color:#4A2370"></stop>
+                </linearGradient>
+                <circle class="point-of-interest" cx="${x}" cy="${y}" r="9.8"></circle>`;
+
+    const point = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+//    const point = document.createElement('g');
+    point.id = `p_x5F_${index}`;
+    point.innerHTML = strPoint;
+    const circles = point.querySelectorAll("circle");
+    circles[0].style.fill = `url(mySVGID_${index}_1)`;
+    circles[2].style.fill = `url(mySVGID_${index}_2)`;
+    document.querySelector('#points').appendChild(point);
+
+    const style1 = document.createElement('style');
+}
+
 
 buttonPlus.addEventListener("click", clickOnPlus);
 buttonMinus.addEventListener("click", clickOnMinus);
@@ -116,3 +147,7 @@ svgmap.addEventListener("click", clickOnMap);
 initArrayPoints();
 initViewBox();
 rescaleSVG();
+addPoint(100,100,20);
+addPoint(100,200,21);
+addPoint(100,300,22);
+
